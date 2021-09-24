c2ab607ad736543d5854eb0e0835e7b30437f2f3
Grizzly1987 <90217368+Grizzly1987@users.noreply.github.com>
2021-09-24 18:44:39 +0200

    Create config.js

---
+++ config.js
@@ -0,0 +1,37 @@
+{
+    "useName": false,
+    "prefix": "!",
+    "delete_command_usage": true,
+    "token": "",
+    "staff_role_id": "",
+    "log_channel": "",
+    "logo_url": "https://i.ibb.co/GTMkx63/FO71-BD2-CF77-C7-rhinoo3d.jpg",
+    "adminOverride": true,
+    "ownerOverride": true,
+    "maximalWarns": 3,
+    "muteRole": "",
+    "main_database": {
+        "host": "mysql-mariadb-22-104.zap-hosting.com",
+        "port": 3306,
+        "user": "zap805597-1",
+        "database": "zap805597-1",
+        "password": "P3WYsME8XhuBYdbe"
+    },
+    "ingame_database": {
+        "host": "mysql-mariadb-22-104.zap-hosting.com",
+        "port": 3306,
+        "user": "zap805597-1",
+        "database": "zap805597-1",
+        "password": "P3WYsME8XhuBYdbe"
+    },
+    "embed": {
+        "url": "https://synchrov.eu",
+        "author": "SynchroV",
+        "footer": "Your SynchroV-Team",
+        "command_usage": "Command Usage"
+    },
+    "activity": {
+        "activity": "SynchroV RP",
+        "type" : "WATCHING"
+    }
+}
